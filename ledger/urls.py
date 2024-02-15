5bf35d292e975e1b332ff1b32b1b1a141f7f4335
“Sophia <sophia.salta@student.ateneo.edu>
2024-02-15 22:14:49 +0800

    add ledger

---
+++ ledger/urls.py
@@ -0,0 +1,12 @@
+#recipebook/ledger/urls.py
+
+from django.urls import path
+from .views import recipes, recipe1, recipe2
+
+urlpatterns = [
+    path('recipes/list', recipes, name='recipes'),
+    path('recipe/1', recipe1, name='recipe 1'),
+    path('recipe/2', recipe2, name='recipe 2'),
+]
+
+app_name = 'ledger'
