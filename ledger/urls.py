4ab5cdbc583fef7809f855c5b01c0ec29f2e506c
Sophia Salta <sophia.salta@student.ateneo.edu>
2024-03-14 22:08:11 +0800

    fixed login/logout flow

--- ledger/urls.py
+++ ledger/urls.py
@@ -4,7 +4,7 @@ from django.urls import path
 from .views import RecipeDetailView, RecipeListView
 
 urlpatterns = [
-    path('list/', RecipeListView.as_view(), name='list'),
+    path('ledger/list/', RecipeListView.as_view(), name='list'),
     path('list/<int:pk>/detail/', RecipeDetailView.as_view(), name='recipe-detail'),
 ]
 
