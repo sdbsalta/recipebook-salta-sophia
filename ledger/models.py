fd3f1277df9ecd9b87cb327ba91133affd87c139
Sophia Salta <sophia.salta@student.ateneo.edu>
2024-03-15 15:15:22 +0800

    bio

--- ledger/models.py
+++ ledger/models.py
@@ -54,8 +54,4 @@ class RecipeIngredient(models.Model):
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=50)
-    short_bio = models.TextField(
-        validators=[
-            MinLengthValidator(255, "Bio must be at least 255 characters long.")
-        ]
-    )
+    bio = models.TextField(validators=[MinLengthValidator(255, "Bio must be at least 255 characters long.")])
